174e90e438a3cd1a46b048c8c11f5402f93d49d5
Vinu Ilangovan <vinuilangovan@Vinus-MacBook-Pro.local>
2012-03-27 18:40:24 -0500

    Initial Commit

---
+++ ViCalc Pro/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ViCalc Pro
+//
+//  Created by Vinu Ilangovan on 3/27/12.
+//  Copyright (c) 2012 VinuIlangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViCalc Pro/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViCalc Pro
+//
+//  Created by Vinu Ilangovan on 3/27/12.
+//  Copyright (c) 2012 VinuIlangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
