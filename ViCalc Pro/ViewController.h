4a0b6b13407b3d031324569cd7651122607789e0
Vinu Ilangovan <vinuilangovan@Vinus-MacBook-Pro.local>
2014-10-20 20:00:07 -0500

    First Commit

--- ViCalc Pro/ViewController.h
+++ ViCalc Pro/ViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UIScrollView *tableScroll;
+}
 
+- (IBAction)basicCalcButton:(id)sender;
+- (IBAction)aboutPageButton:(id)sender;
+- (IBAction)randomNumberButton:(id)sender;
+- (IBAction)topIntegralsButton:(id)sender;
+- (IBAction)volAreaButton:(id)sender;
+- (IBAction)trigLawsButton:(id)sender;
+- (IBAction)trigUnitCircleButton:(id)sender;
+- (IBAction)semesterGradeCalcButton:(id)sender;
+- (IBAction)quadraticEquationSolverButton:(id)sender;
+- (IBAction)averageButton:(id)sender;
+- (IBAction)primeNumbersButton:(id)sender;
+- (IBAction)gcfandLCMButton:(id)sender;
 @end
--- ViCalc Pro/ViewController.m
+++ ViCalc Pro/ViewController.m
@@ -7,15 +7,108 @@
 //
 
 #import "ViewController.h"
+#import "AboutPage.h"
+#import "BasicCalculator.h"
+#import "RandomNumberGenerator.h"
+#import "TopIntegrals.h"
+#import "VolAreaEqns.h"
+#import "TrigLaws.h"
+#import "TrigUnitCircle.h"
+#import "SemesterGradeCalc.h"
+#import "QuadraticEquationSolver.h"
+#import "Average.h"
+#import "PrimeNumbers.h"
+#import "GCFandLCM.h"
 
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
+- (IBAction)quadraticEquationSolverButton:(id)sender{
+    QuadraticEquationSolver *second =[[QuadraticEquationSolver alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)averageButton:(id)sender{
+    Average *second =[[Average alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)primeNumbersButton:(id)sender{
+    PrimeNumbers *second =[[PrimeNumbers alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)gcfandLCMButton:(id)sender{
+    GCFandLCM *second =[[GCFandLCM alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)semesterGradeCalcButton:(id)sender
+{
+    SemesterGradeCalc *second =[[SemesterGradeCalc alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)trigUnitCircleButton:(id)sender
+{
+    TrigUnitCircle *second =[[TrigUnitCircle alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)trigLawsButton:(id)sender
+{
+    TrigLaws *second =[[TrigLaws alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)volAreaButton:(id)sender
+{
+    VolAreaEqns *second =[[VolAreaEqns alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)topIntegralsButton:(id)sender
+{
+    TopIntegrals *second =[[TopIntegrals alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)randomNumberButton:(id)sender
+{
+    RandomNumberGenerator *second =[[RandomNumberGenerator alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+}
+
+- (IBAction)basicCalcButton:(id)sender
+{
+    BasicCalculator *second =[[BasicCalculator alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+	[self presentModalViewController:second animated:YES];
+} 
+
+- (IBAction)aboutPageButton:(id)sender
+{
+	AboutPage *second =[[AboutPage alloc] initWithNibName:nil bundle:nil];
+    second.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+	[self presentModalViewController:second animated:YES];
+}
+
+
+
 - (void)viewDidLoad
 {
+    [tableScroll setScrollEnabled:YES];
+	[tableScroll setContentSize:CGSizeMake(255, 500)];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
